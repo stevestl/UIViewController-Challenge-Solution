a30e9cf7ee0793c3b1d34e311fe63b5ea8793a7b
Steve Kelley <steve@kelleymatthews.net>
2014-06-18 10:55:30 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Steve Kelley on 6/18/14.
+//  Copyright (c) 2014 Steve Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Steve Kelley on 6/18/14.
+//  Copyright (c) 2014 Steve Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
